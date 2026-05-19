fd6148ddaa4c7a4297ba1ac313352afec5a13767
Jacob Hummer <jcbhmr@outlook.com>
2026-05-19 04:47:47 +0000

    save

--- crossexec_test.go
+++ crossexec_test.go
@@ -3,19 +3,15 @@
 package crossexec_test
 
 import (
-	"go.jcbhmr.com/crossexec"
+	_ "go.jcbhmr.com/crossexec"
 )
 
 // [./examples/go-version]
 //
 // [./examples/go-version]: ./examples/go-version
-func ExampleExec_goVersion() {
-	_ = crossexec.Exec
-}
+func ExampleExec_goVersion() {}
 
 // [./examples/extract-and-exec]
 //
 // [./examples/extract-and-exec]: ./examples/extract-and-exec
-func ExampleExec_extractAndExec() {
-	_ = crossexec.Exec
-}
+func ExampleExec_extractAndExec() {}
